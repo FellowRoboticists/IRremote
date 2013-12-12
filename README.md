6c2e9dcb5b1eff89b5a08cb7c92e17a856f458c9
Dave Sieh <dj0hnve@gmail.com>
2013-12-11 19:05:29 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+IRremote Library
+================
+
+This Arduino library allows you to read an Infrared remote sensor. This is the
+same kind of sensor that is inside your TV to see the signals from your
+TV remote.
---
+++ src/sketch.ino
@@ -0,0 +1,11 @@
+#include <IRremote.h>
+
+const int irPin = A3;   // analog input pin 3 (digital 17)
+IRrecv irrecv(irPin);     // create the IR receive object
+
+void setup() {
+  irrecv.enableIRIn(); // Start the ir receiver
+}
+
+void loop() {
+}
